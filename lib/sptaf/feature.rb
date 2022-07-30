ff9460b3d9c63524158c1630e5ba0686c4e874c6
Ken Coar <ken.coar@gmail.com>
2022-07-30 01:26:39 -0400

    Just create feature files for testing classes, add location features, and rename the command interface source file.

---
+++ lib/sptaf/feature.rb
@@ -0,0 +1,43 @@
+#--
+# Copyright © 2022 Ken Coar
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You
+# may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.  See the License for the specific language governing
+# permissions and limitations under the License.
+#++
+# frozen_string_literal: true
+
+require_relative('../sptaf')
+require('byebug')
+
+# @!macro doc.TAF.module
+module TAF
+
+  #
+  class Feature
+
+    include(Mixins::Container)
+
+    def initialize(*args, **kwargs)
+      self.initialize_thing(*args, **kwargs)
+      self.static!
+    end                         # def initialize(*args, **kwargs)
+
+  end                           # class Feature
+
+  nil
+end                             # module TAF
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: nil
+# eval: (if (intern-soft "fci-mode") (fci-mode 1))
+# End:
