0bc8bd095bf71435746e4083e6c9b53463f60343
Ken Coar <ken.coar@gmail.com>
2023-12-31 02:25:21 -0500

    Tiny forward motion in testing

--- test/unit/truthify_test.rb
+++ test/unit/truthify_test.rb
@@ -4,7 +4,7 @@ require('test/unit')
 class Test_Truthify < Test::Unit::TestCase
 
   def setup
-    
+    @samples		= YAML.load('../fixtures/truthify.yaml')
   end                           # def setup
 
   def teardown
@@ -13,9 +13,10 @@ class Test_Truthify < Test::Unit::TestCase
 
   #
   def test_true
-    assert('truthify(true) failed') do
-      TAGF.truthify(true)
-    end
+    @samples['true'].each do |testval|
+      assert(format('truthify(%s) => true failed', testval.inspect) do
+      TAGF.truthify(testval)
+    end                         # @samples['true'].each do
   end                           # def test_true
 
 end                             # class Test_Truthify
